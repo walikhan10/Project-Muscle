7c6abec7d8eb5c54e0e619ef96789bdd7a8623e9
walikhan10 <walikhan10@gmail.com>
2021-04-14 16:20:13 -0400

    Dark Mode and Themes!

--- Final/app.js
+++ Final/app.js
@@ -4,7 +4,7 @@ fetch("https://type.fit/api/quotes")
   })
   .then(function(data) {
     let index = Math.floor(Math.random() * data.length);
-   document.getElementById('frame').innerHTML = data[index].text + " -" + (data[index].author ? data[index].author : "Anonymous");
+   document.getElementById('frame').innerHTML = '"' + data[index].text + '"' + " -" + (data[index].author ? data[index].author : "Anonymous");
   });
   
 
@@ -13,3 +13,28 @@ fetch("https://type.fit/api/quotes")
              //uncomment this line for normal hover highlighting
             $('.image').maphilight();
         });   
+
+
+
+        function setTheme(themeName) {
+          localStorage.setItem('theme', themeName);
+          document.documentElement.className = themeName;
+      }
+
+      // function to toggle between light and dark theme
+      function toggleTheme() {
+          if (localStorage.getItem('theme') === 'theme-dark') {
+              setTheme('theme-light');
+          } else {
+              setTheme('theme-dark');
+          }
+      }
+
+      // Immediately invoked function to set the theme on initial load
+      (function () {
+          if (localStorage.getItem('theme') === 'theme-dark') {
+              setTheme('theme-dark');
+          } else {
+              setTheme('theme-light');
+          }
+      })();
