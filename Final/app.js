8b08281068949d02c7846e31748e26ec4f8b5421
walikhan10 <walikhan10@gmail.com>
2021-04-29 16:01:04 -0400

    Implemented 4 APIs

--- Final/app.js
+++ Final/app.js
@@ -1,3 +1,5 @@
+
+
 fetch("https://type.fit/api/quotes")
    .then(function(response) {
     return response.json();
@@ -8,12 +10,7 @@ fetch("https://type.fit/api/quotes")
   });
   
 
-  $(document).ready(function () {
-            //$('.image').maphilight({ alwaysOn: true });
-             //uncomment this line for normal hover highlighting
-            $('.image').maphilight();
-        });   
-
+  
 
 
         function setTheme(themeName) {
@@ -38,3 +35,135 @@ fetch("https://type.fit/api/quotes")
               setTheme('theme-light');
           }
       })();
+
+
+    
+
+
+
+    //wger api
+
+
+   
+
+
+     
+
+      $(function ready(){
+        $('.image').maphilight();
+
+        $("area").on("click", async (e) => {
+          let muscle = e.target.id;
+          await getExercise(muscle);
+
+        });        
+
+
+        $("#search").on("click",  (e) => {
+          e.preventDefault()
+          exercise($('#searchTerm').val())
+          ///alert("hi")
+          
+        })
+
+     
+      });
+
+
+
+      
+       
+     
+    
+
+// list all categories of functions, and then 
+
+
+    var API_URL = `https://wger.de/api/v2/exercise/?format=json&language=2&limit=100&category=`
+    
+    async function getExercise(muscle){
+
+        console.log("hello " + muscle)
+
+      const response = await fetch(API_URL + muscle);
+      const data = await response.json();
+      var i, x = "";
+      for (i = 0; i < data.results.length; i++) {
+         x += 
+         
+         `
+
+
+  <article class="message is-dark">
+  <div class="message-header">
+    <p> ${data.results[i].name} </p>
+  </div>
+  <div class="message-body">
+  ${data.results[i].description}
+  </div>
+</article>`
+
+       }
+      document.getElementById("workouts").innerHTML = x
+   
+    
+    }
+
+
+
+// let searchButton = document.querySelector('#square')
+
+// searchButton.addEventListener("click", ()=>{
+//   alert("hello")
+//   console.log("button pressed")
+//   sendApiRequest()
+// })
+
+
+//An asynchronous function to fetch data from the API.
+async function sendApiRequest(){
+  let response = await fetch(``);
+  console.log(response)
+}
+
+
+//function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
+function useApiData(data){
+
+}
+      
+
+function exercise(input) {
+  var myHeaders = new Headers();
+myHeaders.append("x-app-id", "1889c865");
+myHeaders.append("x-app-key", "668fe1e1f50e5221f889d8e791685eea");
+myHeaders.append("Content-Type", "application/json");
+console.log(input)
+var raw = JSON.stringify({
+  "query": input
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://trackapi.nutritionix.com/v2/natural/exercise", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    console.log(result.exercises[0].nf_calories)
+
+    document.getElementById('test2').textContent= result.exercises[0].nf_calories;
+
+   
+
+
+  })
+
+}
+
+// exercise($('#searchTerm'))
+
+
