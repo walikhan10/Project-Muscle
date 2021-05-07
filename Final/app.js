3f3687b21cb8232066cc6c6c2374423fdfd74b9d
walikhan10 <walikhan10@gmail.com>
2021-05-07 16:57:25 -0400

    more updates

--- Final/app.js
+++ Final/app.js
@@ -1,105 +1,105 @@
 
 
 fetch("https://type.fit/api/quotes")
-   .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data) {
+  .then(function (data) {
     let index = Math.floor(Math.random() * data.length);
-   document.getElementById('frame').innerHTML = '"' + data[index].text + '"' + " -" + (data[index].author ? data[index].author : "Anonymous");
+    document.getElementById('frame').innerHTML = '"' + data[index].text + '"' + " -" + (data[index].author ? data[index].author : "Anonymous");
   });
-  
-
-  
-
-
-        function setTheme(themeName) {
-          localStorage.setItem('theme', themeName);
-          document.documentElement.className = themeName;
-      }
-
-      // function to toggle between light and dark theme
-      function toggleTheme() {
-          if (localStorage.getItem('theme') === 'theme-dark') {
-              setTheme('theme-light');
-          } else {
-              setTheme('theme-dark');
-          }
-      }
-
-      // Immediately invoked function to set the theme on initial load
-      (function () {
-          if (localStorage.getItem('theme') === 'theme-dark') {
-              setTheme('theme-dark');
-          } else {
-              setTheme('theme-light');
-          }
-      })();
-
-
-    
 
 
 
-    //wger api
 
 
-   
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-light');
+  } else {
+    setTheme('theme-dark');
+  }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark');
+  } else {
+    setTheme('theme-light');
+  }
+})();
 
 
-     
-
-      $(function ready(){
-        $('.image').maphilight();
-
-        $("area").on("click", async (e) => {
-          let muscle = e.target.id;
-          await getExercise(muscle);
-          
-        });        
 
 
-        $("#search").on("click",  (e) => {
-          e.preventDefault()
-          exercise($('#searchTerm').val())
-        ///alert("hi")
-
-        })
-
-        $("area").on("click", async (e) => {
-          let muscle = e.target.id;
-          await getExercise(muscle);
-          
-        }); 
-      
 
 
-      });
-
-     
+//wger api
 
 
-      
-       
-     
-    
+
+
+
+
+
+$(function ready() {
+  $('.image').maphilight();
+
+  $("area").on("click", async (e) => {
+    let muscle = e.target.id;
+    await getExercise(muscle);
+
+  });
+
+
+  $("#search").on("click", (e) => {
+    e.preventDefault()
+    exercise($('#searchTerm').val())
+    ///alert("hi")
+
+  })
+
+  $("area").on("click", async (e) => {
+    let muscle = e.target.id;
+    await getExercise(muscle);
+
+  });
+
+
+
+});
+
+
+
+
+
+
+
+
 
 // list all categories of functions, and then 
 
 
-    var API_URL = `https://wger.de/api/v2/exercise/?format=json&language=2&limit=100&category=`
-    
-    async function getExercise(muscle){
+var API_URL = `https://wger.de/api/v2/exercise/?format=json&language=2&limit=100&category=`
 
-        console.log("hello " + muscle)
+async function getExercise(muscle) {
 
-      const response = await fetch(API_URL + muscle);
-      const data = await response.json();
-      var i, x = "";
-      for (i = 0; i < data.results.length; i++) {
-         x += 
-         
-         `
+  console.log("hello " + muscle)
+
+  const response = await fetch(API_URL + muscle);
+  const data = await response.json();
+  var i, x = "";
+  for (i = 0; i < data.results.length; i++) {
+    x +=
+
+      `
 
 <br>
   <article id = "card" class="message is-dark">
@@ -111,42 +111,104 @@ fetch("https://type.fit/api/quotes")
   </div>
 </article>`
 
-       }
-      document.getElementById("workouts").innerHTML = x
-   
-    
-    }
+  }
+  document.getElementById("workouts").innerHTML = x
+
+
+}
 
 
 
 
 function exercise(input) {
   var myHeaders = new Headers();
-myHeaders.append("x-app-id", "1889c865");
-myHeaders.append("x-app-key", "668fe1e1f50e5221f889d8e791685eea");
-myHeaders.append("Content-Type", "application/json");
-//console.log(input)
-var raw = JSON.stringify({
-  "query": input
-});
+  myHeaders.append("x-app-id", "1889c865");
+  myHeaders.append("x-app-key", "668fe1e1f50e5221f889d8e791685eea");
+  myHeaders.append("Content-Type", "application/json");
+  //console.log(input)
+  var raw = JSON.stringify({
+    "query": input
+  });
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
 
-fetch("https://trackapi.nutritionix.com/v2/natural/exercise", requestOptions)
-  .then(response => response.json())
-  .then(result => {
-    console.log(result.exercises[0].nf_calories)
+  fetch("https://trackapi.nutritionix.com/v2/natural/exercise", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result.exercises[0].nf_calories)
 
-    document.getElementById('test2').textContent= result.exercises[0].nf_calories + " kcal";
+      document.getElementById('test2').textContent = result.exercises[0].nf_calories + " kcal";
 
 
 
-  })
+    })
+
 
 }
+
+function recipe() {
+
+  let searchButton = document.querySelector("#walirec")
+  const searchForm = document.querySelector("#rec-form");
+
+
+  //Add an event listener to the button that runs the function sendApiRequest when it is clicked
+  searchButton.addEventListener("click", () => {
+    console.log("button pressed")
+    sendApiRequest()
+  })
+
+
+  // searchForm.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   searchQuery = e.target.querySelector("#searchItem").value;
+  //   sendApiRequest()
+  // });
+
+
+  //An asynchronous function to fetch data from the API.
+  async function sendApiRequest() {
+    let APP_ID = "f1cf6ce0"
+    let APP_key = "e40345273a7eed9cf09730927d58da4e"
+    let response = await fetch(`https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_key}&to=50`);
+    console.log(response)
+    let data = await response.json()
+    console.log(data)
+    useApiData(data)
+  }
+
+
+  //function that does something with the data received from the API. The name of the function should be customized to whatever you are doing with the data
+  function useApiData(data) {
+
+    document.querySelector("#rec-content").innerHTML = `
+
+  
+      
+     <div class="card">
+     <img src="${data.hits[0].recipe.image}" alt="Avatar" >
+     <div class="container">
+       <h4><b>${data.hits[0].recipe.label}</b></h4>
+       <p> Calories:${data.hits[0].recipe.calories.toFixed(2)}</p>
+       <button id = "rec-button"><a href="${data.hits[0].recipe.url}">Search</a></button>
+     </div>
+   </div>
+
+
+       
+
+   
+    `
+  }
+
+
+
+  recipe()
+}
+
 
--- Final/recipe/main.js
+++ Final/recipe/main.js
@@ -1,225 +1,106 @@
-const searchForm = document.querySelector('form');
-const searchResultDiv = document.querySelector('.search-result');
-const searchIcon = document.querySelector('.icon');
-const container = document.querySelector('.container');
-let searchQuery = '';
-const APP_ID = 'f1cf6ce0';
-const APP_key = 'e40345273a7eed9cf09730927d58da4e';
-const baseURL = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_key}`;
-
-
-searchIcon.addEventListener('click',(e)=>{
-    e.preventDefault();
-    searchQuery = document.querySelector('input').value;
-    fetchAPI();
-    console.log('test icon',e);
-});
-
-searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    searchQuery = e.target.querySelector('input').value;
-    fetchAPI();
-});
-
-async function fetchAPI() {
-    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}`;
-    const response = await fetch(baseURL);
-    const data = await response.json()
-    generateHTML(data.hits);
-    console.log(data);
-}
-
-function generateHTML(results) {
-    container.classList.remove('initial');
-    let generatedHTML = '';
-    results.map(result => {
-        generatedHTML +=
-            `
-        <div class="item">
-            <img src="${result.recipe.image}" alt="img">
-            <div class="flex-container">
-                <h1 class="title">${result.recipe.label}</h1>
-                <a class="view-button" href="${result.recipe.url}" target="blank">View Recipe</a>
-            </div>
-            <p class="item-data">Calories : ${parseInt(result.recipe.calories)}</p>           
-            <p class="item-data">Diet label  : ${result.recipe.dietLabels.length > 0 ? result.recipe.dietLabels.length : 'No Data Found :('}</p>           
-            <p class="item-data">Health Label : ${result.recipe.healthLabels}</p>           
-
-        
-        </div>
-        `
-    })
-    searchResultDiv.innerHTML = generatedHTML;
-}
-
-
-
-var foodSearch = [];
-
-$("#foodList").on('keyup', function (event){
-    event.preventDefault();
-var myHeaders = new Headers();
-myHeaders.append("x-app-id", "1889c865");
-myHeaders.append("x-app-key", "668fe1e1f50e5221f889d8e791685eea");
-myHeaders.append("Content-Type", "application/json");
-
-var raw = JSON.stringify({
-  "query": "cookie"
-});
-
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
+var words = ["lowtide", "clam", "coral", "boat", "coast", "hermit", "fish"];
+//random string from array
+const randomNum = () => {
+    return Math.floor(Math.random() * words.length);
 };
-fetch("https://trackapi.nutritionix.com/v2/search/instant", requestOptions)
-  .then(response => response.json())
- // .then(result => console.log(result.common))
-  .then(result => {
+//consolidated to variable
+let chosenWord = words[randomNum()];
+//open array for all the right letters chosen
+let rightWordArray = [];
+// open array for all the letters guessed/wrong
+let wrongWordArray = [];
+// open array for the chosen words which helps generate underscores
+let underScore = [];
+let docUnderScore = document.getElementsByClassName("underline");
+let docGuess = document.getElementsByClassName("guessedlettersdiv");
+let wins = 0;
+let winsCount = document.getElementsByClassName("winscount");
+let guessremainingNum = 15;
+let guessremainingNumSpan = document.getElementById("guessremainingNum");
+let imgDiv = document.getElementById("imgdivid");
 
-    
- for (var i = 0; i < result.common.length; i++){
-            console.log( result.common[i].food_name)
-         foodSearch.push(result.common[i].food_name);
-  }
+//not working properly unable to reload image and create a new word rather than existing
+function resetFunc() {
+    chosenWord = words[randomNum()];
+    imgDiv.removeChild(imgDiv.childNodes[2]);
+    newPicture();
+    guessremainingNum = 15;
+    guessremainingNumSpan.innerHTML = guessremainingNum;
+    rightWordArray = [];
+    wrongWordArray = [];
+    underScore = [];
+    docUnderScore[0].innerHTML = generateUnderscore().join(" ");
+}
 
-  })
-  .then(console.log(foodSearch))
-  .catch(error => console.log('error', error));
+function randomTrivia() { }
 
+function generateUnderscore() {
+    for (let i = 0; i < chosenWord.length; i++) {
+        underScore.push("_");
+    }
+    return underScore;
+}
+function newPicture() {
+    if (chosenWord === words[0]) {
+        var img = document.createElement("img");
+        img.src = "images/lowtide.jpg";
+        imgDiv.appendChild(img);
+    } else if (chosenWord === words[1]) {
+        var img = document.createElement("img");
+        img.src = "images/clam.jpg";
+        imgDiv.appendChild(img);
+    } else if (chosenWord === words[2]) {
+        var img = document.createElement("img");
+        img.src = "images/coral.jpg";
+        imgDiv.appendChild(img);
+    } else if (chosenWord === words[3]) {
+        var img = document.createElement("img");
+        img.src = "images/boat.jpg";
+        imgDiv.appendChild(img);
+    } else if (chosenWord === words[4]) {
+        var img = document.createElement("img");
+        img.src = "images/coast.jpg";
+        imgDiv.appendChild(img);
+    } else if (chosenWord === words[5]) {
+        var img = document.createElement("img");
+        img.src = "images/hermit.jpg";
+        imgDiv.appendChild(img);
+    } else if (chosenWord === words[6]) {
+        var img = document.createElement("img");
+        img.src = "images/fish.jpg";
+        imgDiv.appendChild(img);
+    }
+}
 
+// creating the function to minus the number of guesses when a letter is pushed to wrongWordArray
+function wrongGuess() {
+    guessremainingNum--;
+    guessremainingNumSpan.innerHTML = guessremainingNum;
 
-})
+    if (guessremainingNum === 0) {
+        alert("You Lose 😢");
+        location.reload();
+    }
+}
 
-// for (var i = 0; i < response.common.length; i++){
-//     //             console.log("testing" + response.common[i].food_name)
-//     //             foodSearch.push(response.common[i].food_name);
+document.addEventListener("keyup", function () {
+    let keyword = String.fromCharCode(event.keyCode).toLowerCase();
+    if (chosenWord.indexOf(keyword) > -1) {
+        rightWordArray.push(keyword);
+        underScore[chosenWord.indexOf(keyword)] = keyword;
+        docUnderScore[0].innerHTML = underScore.join(" ");
+        if (underScore.join("") == chosenWord) {
+            alert("You Win 😊");
+            wins++;
+            winsCount.innerHTML = wins;
+            resetFunc();
+        }
+    } else {
+        wrongWordArray.push(keyword);
+        wrongGuess();
+    }
+    //creates the letters that are wrongly guessed in the DOM
+    docGuess[0].innerHTML = wrongWordArray.join(" ");
+});
 
-// }
-
-
-
-
-
-
-// var substringMatcher = function(strs) {
-//     return function findMatches(q, cb) {
-//       var matches, substrRegex;
-   
-//       // an array that will be populated with substring matches
-//       matches = [];
-   
-//       // regex used to determine if a string contains the substring `q`
-//       substrRegex = new RegExp(q, 'i');
-   
-//       // iterate through the pool of strings and for any string that
-//       // contains the substring `q`, add it to the `matches` array
-//       $.each(strs, function(i, str) {
-//         if (substrRegex.test(str)) {
-//           // the typeahead jQuery plugin expects suggestions to a
-//           // JavaScript object, refer to typeahead docs for more info
-//           matches.push({ value: str });
-//         }
-//       });
-   
-//       cb(matches);
-//     };
-//   };
-   
-  
-   
-//   $('#search-class .typeahead').typeahead({
-//     hint: true,
-//     highlight: true,
-//     minLength: 1
-//   },
-//   {
-//     name: 'foodList',
-//     displayKey: 'value',
-//     source: substringMatcher(foodSearch)
-//   });
-
-
-// console.log("TEST")
-
-// var foodSearch = [];
-// var settings = {
-//     "async": true,
-//     "crossDomain": true,
-//     "url": "https://trackapi.nutritionix.com/v2/search/instant?query=cheese",
-//     "method": "GET",
-//     "headers": {
-//       "x-app-key": "1d07da98cfc1746cbbbf2f301a4ca900",
-//       "x-app-id": "9beb2ceb",
-//       "Cache-Control": "no-cache",
-//       "Postman-Token": "36850c71-13e6-4bd9-97bd-e470293f1f6d"
-//     }
-//   }
-
-
-//   $("#foodList").on('keyup', function (event) {
-//     event.preventDefault();
-//         console.log($(this).val());
-//         var food = $(this).val();
-//         settings = {
-//             "async": true,
-//             "crossDomain": true,
-//             "url": `https://trackapi.nutritionix.com/v2/search/instant?query=${food}`,
-//             "method": "GET",
-//             "headers": {
-//               "x-app-key": "668fe1e1f50e5221f889d8e791685eea",
-//               "x-app-id": "1889c865",
-//               "Cache-Control": "no-cache",
-//               "Postman-Token": "36850c71-13e6-4bd9-97bd-e470293f1f6d"
-//             }
-//           }
-//       $.ajax(settings).done(function (response) {
-//         console.log(response);
-    
-//         for (var i = 0; i < response.common.length; i++){
-//             console.log("testing" + response.common[i].food_name)
-//             foodSearch.push(response.common[i].food_name);
-    
-//         }
-//         console.log(foodSearch)
-//       });
-      
-//   })
-
-// var substringMatcher = function(strs) {
-//     return function findMatches(q, cb) {
-//       var matches, substrRegex;
-   
-//       // an array that will be populated with substring matches
-//       matches = [];
-   
-//       // regex used to determine if a string contains the substring `q`
-//       substrRegex = new RegExp(q, 'i');
-   
-//       // iterate through the pool of strings and for any string that
-//       // contains the substring `q`, add it to the `matches` array
-//       $.each(strs, function(i, str) {
-//         if (substrRegex.test(str)) {
-//           // the typeahead jQuery plugin expects suggestions to a
-//           // JavaScript object, refer to typeahead docs for more info
-//           matches.push({ value: str });
-//         }
-//       });
-   
-//       cb(matches);
-//     };
-//   };
-   
-  
-   
-//   $('#the-basics .typeahead').typeahead({
-//     hint: true,
-//     highlight: true,
-//     minLength: 1
-//   },
-//   {
-//     name: 'foodList',
-//     displayKey: 'value',
-//     source: substringMatcher(foodSearch)
-//   });
+resetFunc();
